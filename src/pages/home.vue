a08455fea2ca3e82a2a48afcadd84ebf06670982
Woochang1132 <show4031132@gmail.com>
2025-03-18 17:12:20 +0900

    [vue3-firebase] > 404페이지, ...path.vue 파일로 설정하기 및 route정보 확인

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="text-h4">Home</div>
+  <div>
+    {{ $route.name }}<br />
+    {{ $route.meta }}
+  </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
+
+<route lang="yaml">
+name: home-page
+meta:
+  requiresAuth: true
+</route>
