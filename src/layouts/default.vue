2f392f1d02d880c23b09c41f4f469010a598e99b
Woochang1132 <show4031132@gmail.com>
2025-03-19 15:28:45 +0900

    [vue3 - firebase] > default 레이아웃 설정을 통해서 header 구성완료

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>
-    <div class="text-h2">Default Layout</div>
-    <router-view />
-  </div>
+  <q-layout view="hHh lpR fFf">
+    <q-header bordered class="bg-white text-grey-9">
+      <q-toolbar>
+        <q-btn flat dense to="/">
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="/c_logo.jpg" />
+            </q-avatar>
+            Community-center
+          </q-toolbar-title>
+        </q-btn>
+        <q-space />
+        <q-btn stretch flat label="Home" to="/home" />
+        <q-btn
+          stretch
+          flat
+          label="수강하기"
+          href="https://google.com"
+          target="_blank"
+        />
+        <q-btn
+          stretch
+          flat
+          label="온라인 강의"
+          href="https://edu.gymcoding.co"
+          target="_blank"
+        />
+        <q-btn
+          stretch
+          flat
+          label="유튜브"
+          href="https://youtube.com/@gymcoding"
+          target="_blank"
+        />
+        <q-separator class="q-my-md q-mr-md" vertical />
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="로그인 / 회원가입"
+        ></q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
