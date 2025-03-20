e4c611f524f9668ce3da68dc6aea9aeb6d20b5b2
Woochang1132 <show4031132@gmail.com>
2025-03-20 11:40:22 +0900

    [vue3-firebase] > 로그인, 회원가입, 비밀번호 찾기 => dialog 구성 진행중

---
+++ src/components/auth/AuthDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-dialog
+    :model-value="modelValue"
+    @update:model-value="val => $emit('update:modelValue', val)"
+    transition-show="none"
+    transition-hide="none"
+  >
+    <q-card :style="{ width: '400px' }">
+      <q-card-section class="flex">
+        <q-space></q-space>
+        <q-btn icon="close" flat round dense v-close-popup></q-btn>
+      </q-card-section>
+      <q-card-section>
+        <SignInForm />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import SignInForm from './SignInForm.vue';
+// import SignUpForm from './SignUpForm.vue';
+// import FindPasswordForm from './FindPasswordForm.vue';
+
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/auth/FindPasswordForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>FindPasswordForm</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
