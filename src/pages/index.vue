682248d4d61338108087e94e15570c8dbdf9c494
Woochang1132 <show4031132@gmail.com>
2025-03-22 15:38:26 +0900

    [vue3-firebase] > 커뮤니티 목록, q-list로 구성하기

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,20 +2,27 @@
   <q-page padding>
     <div class="text-h4">커뮤니티 목록</div>
     <section class="q-gutter-y-sm q-mt-lg">
-      <q-card v-for="id in 100" :key="id" @click="goPostDetails(id)">
-        <q-card-section>{{ id }}번 게시글</q-card-section>
-      </q-card>
+      <q-list bordered separator>
+        <q-item v-for="post in posts" :key="post.id" clickable>
+          <q-item-section>Single line item</q-item-section>
+        </q-item>
+      </q-list>
     </section>
   </q-page>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router';
 
-const router = useRouter();
-const goPostDetails = id => {
-  router.push(`/posts/${id}`);
-};
+// const router = useRouter();
+// const goPostDetails = id => {
+//   router.push(`/posts/${id}`);
+// };
+
+const posts = Array.from(Array(20), (_, index) => ({
+  id: index,
+  title: 'Vue3 Firebase 강의' + index,
+}));
 </script>
 
 <style lang="scss" scoped></style>
