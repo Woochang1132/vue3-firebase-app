1598d4955e976d46e0df3d675dbf93449a89b8f6
Woochang1132 <show4031132@gmail.com>
2025-03-24 17:38:25 +0900

    [vue3-firebase] > post 상세 페이지, markup 구성하기

--- src/pages/posts/[id]/index.vue
+++ src/pages/posts/[id]/index.vue
@@ -1,12 +1,77 @@
 <template>
-  <div class="text-h4">게시글 상세</div>
-  <ul>
-    <li>
-      <router-link to="/posts/1/edit">수정하기</router-link>
-    </li>
-  </ul>
+  <q-page padding>
+    <q-card class="q-pa-lg">
+      <div class="flex q-mb-md">
+        <q-btn
+          icon="sym_o_arrow_back"
+          flat
+          round
+          dense
+          color="grey"
+          size="16px"
+        />
+        <q-space />
+        <q-btn icon="sym_o_favorite" flat round dense color="red" size="16px" />
+        <q-btn
+          icon="sym_o_bookmark"
+          flat
+          round
+          dense
+          color="blue"
+          size="16px"
+        />
+      </div>
+      <div class="flex items-center">
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+        </q-avatar>
+        <div class="q-ml-md">
+          <div>Woochang</div>
+          <div class="text-grey-6">3일 전</div>
+        </div>
+        <q-space />
+        <q-btn icon="more_horiz" round flat>
+          <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup>
+                <q-item-section>수정하기</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>삭제하기</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </div>
+
+      <div class="q-mt-md text-h5 text-weight-bold">제목입니다</div>
+      <q-separator class="q-my-lg" />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        sint, facere inventore rerum praesentium saepe eveniet fugiat debitis
+        quod non iste porro commodi, ipsam ab a numquam sapiente modi amet?
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        sint, facere inventore rerum praesentium saepe eveniet fugiat debitis
+        quod non iste porro commodi, ipsam ab a numquam sapiente modi amet?
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        sint, facere inventore rerum praesentium saepe eveniet fugiat debitis
+        quod non iste porro commodi, ipsam ab a numquam sapiente modi amet?
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        sint, facere inventore rerum praesentium saepe eveniet fugiat debitis
+        quod non iste porro commodi, ipsam ab a numquam sapiente modi amet?
+        <br />
+      </div>
+    </q-card>
+  </q-page>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
+<route lang="yaml">
+meta:
+  width: 800px
+</route>
