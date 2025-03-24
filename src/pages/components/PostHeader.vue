1f96549ce2e9d5abe8844d3806e00d4bd65c9859
Woochang1132 <show4031132@gmail.com>
2025-03-24 14:14:32 +0900

    [vue3-firebase]  > 레이아웃 3등 분할, PostHeader 구성 진행중

--- src/pages/components/PostHeader.vue
+++ src/pages/components/PostHeader.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="text-h4">커뮤니티 목록</div>
+  <div class="flex items-center q-mb-lg">
+    <div class="text-h5">커뮤니티 목록</div>
+    <q-space />
+    <q-tabs narrow-indicator dense>
+      <q-tab :ripple="false" label="최신순" name="createTime" />
+      <q-tab :ripple="false" label="조회순" name="readCount" />
+      <q-tab :ripple="false" label="좋아요순" name="likeCount" />
+    </q-tabs>
+  </div>
 </template>
 
 <script setup></script>
