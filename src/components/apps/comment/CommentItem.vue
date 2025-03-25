7117705682c3a60945c0868d598b98fbf86d358f
Woochang1132 <show4031132@gmail.com>
2025-03-25 12:17:13 +0900

    [vue3-firebase] > q-card 태그, 공통 컴포넌트로 분리하여 구성하기

---
+++ src/components/apps/comment/CommentItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-item class="q-py-md">
+    <q-item-section side top>
+      <q-avatar size="md">
+        <img src="https://cdn.quasar.dev/img/avatar.png" />
+      </q-avatar>
+    </q-item-section>
+    <q-item-section>
+      <div class="flex text-caption">
+        <span>woochang</span>
+        <span class="q-mx-xs">&middot;</span>
+        <span class="text-grey-6">3일 전</span>
+      </div>
+      <div class="q-mt-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
+        officia placeat eius? Ipsam saepe mollitia eos, odio officiis
+        cupiditate! Quos esse reiciendis magni numquam sint deleniti nemo? Ad,
+        minus aliquam?
+      </div>
+    </q-item-section>
+    <q-item-section side top>
+      <q-btn flat color="grey" icon="sym_o_delete"></q-btn>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/pages/posts/[id]/components/PostDetails.vue
+++ src/pages/posts/[id]/components/PostDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="q-pa-lg">
+  <BaseCard class="q-pa-lg">
     <div class="flex q-mb-md">
       <q-btn
         icon="sym_o_arrow_back"
@@ -62,11 +62,12 @@
       iste porro commodi, ipsam ab a numquam sapiente modi amet?
       <br />
     </div>
-  </q-card>
+  </BaseCard>
 </template>
 
 <script setup>
 import PostIcon from 'src/components/apps/post/PostIcon.vue';
+import BaseCard from 'src/components/base/BaseCard.vue';
 </script>
 
 <style lang="scss" scoped></style>
