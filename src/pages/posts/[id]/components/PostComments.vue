a7eca1ba8ffdf6316b168751eba4cde86568c2bc
Woochang1132 <show4031132@gmail.com>
2025-03-25 11:22:25 +0900

    [vue3-firebase] > post 상세 페이지, 댓글 영역 컴포넌트 구성 완료

--- src/pages/posts/[id]/components/PostComments.vue
+++ src/pages/posts/[id]/components/PostComments.vue
@@ -1,7 +1,64 @@
 <template>
-  <div>PostComments</div>
+  <div>
+    <div class="text-subtitle1 text-weight-bold q-mb-lg">댓글 6</div>
+
+    <div v-if="isActive">
+      <q-input type="textarea" class="bg-white" outlined></q-input>
+      <div class="flex justify-end q-gutter-x-sm q-mt-sm" outlined>
+        <q-btn
+          label="취소"
+          color="dark"
+          unelevated
+          @click="toggleActive"
+        ></q-btn>
+        <q-btn label="등록" color="primary" unelevated></q-btn>
+      </div>
+    </div>
+
+    <q-card v-if="!isActive" @click="toggleActive" class="cursor-pointer">
+      <q-card-section class="flex items-center">
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+        </q-avatar>
+        <div class="text-grey-6 q-ml-lg">댓글을 작성해보세요.</div>
+      </q-card-section>
+    </q-card>
+
+    <q-list class="q-mt-lg bg-white" bordered separator>
+      <q-item>
+        <q-item-section side top>
+          <q-avatar size="md">
+            <img src="https://cdn.quasar.dev/img/avatar.png" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <div class="flex text-caption">
+            <span>woochang</span>
+            <span class="q-mx-xs">&middot;</span>
+            <span class="text-grey-6">3일 전</span>
+          </div>
+          <div class="q-mt-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Consectetur officia placeat eius? Ipsam saepe mollitia eos, odio
+            officiis cupiditate! Quos esse reiciendis magni numquam sint
+            deleniti nemo? Ad, minus aliquam?
+          </div>
+        </q-item-section>
+        <q-item-section side top>
+          <q-btn flat color="grey" icon="sym_o_delete"></q-btn>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const isActive = ref(false);
+const toggleActive = () => {
+  isActive.value = !isActive.value;
+};
+</script>
 
 <style lang="scss" scoped></style>
